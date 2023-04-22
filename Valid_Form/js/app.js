38301f216dc5d8bdf5968c530013bbd36c559e5d
javaldebenito <ja.valdebenitoa@duocuc.cl>
2023-04-22 10:41:53 -0400

    final de clase

--- Valid_Form/js/app.js
+++ Valid_Form/js/app.js
@@ -1,59 +1,71 @@
-var nt1 = 4.5; 
-var nt2 = 2.0; 
-var nt3 = 3.3; 
-var suma = nt1 + nt2 + nt3; 
+// var nt1 = 4.5; 
+// var nt2 = 2.0; 
+// var nt3 = 3.3; 
+// var suma = nt1 + nt2 + nt3; 
  
-if(suma/3>=4){ 
-    console.log("aprobado"); 
+// if(suma/3>=4){ 
+//     console.log("aprobado"); 
  
-} 
-else{ 
-    console.log ("reprobado"); 
+// } 
+// else{ 
+//     console.log ("reprobado"); 
  
-} 
+// } 
  
-//sentencia switch 
+// //sentencia switch 
  
-var vProm  =suma/3; 
-var sw = 0; 
+// var vProm  =suma/3; 
+// var sw = 0; 
  
-if(vProm >=1 && vProm<4){sw=3;} 
-else if(vProm >=4 && vProm<5){ sw=4;} 
-else if(vProm >=5 && vProm<6){ sw=5;} 
-else if(vProm >=6 && vProm<7){ sw=6;} 
+// if(vProm >=1 && vProm<4){sw=3;} 
+// else if(vProm >=4 && vProm<5){ sw=4;} 
+// else if(vProm >=5 && vProm<6){ sw=5;} 
+// else if(vProm >=6 && vProm<7){ sw=6;} 
  
-switch(sw){ 
-    case 3: 
-        console.log("El promedio" + vProm +"es insuficiente"); 
-        break; 
+// switch(sw){ 
+//     case 3: 
+//         console.log("El promedio" + vProm +"es insuficiente"); 
+//         break; 
      
-    case 4: 
-        console.log("El promedio" + vProm +"es suficiente"); 
-        break;   
+//     case 4: 
+//         console.log("El promedio" + vProm +"es suficiente"); 
+//         break;   
       
-    case 5: 
-        console.log("El promedio" + vProm +"es bueno"); 
-        break;    
+//     case 5: 
+//         console.log("El promedio" + vProm +"es bueno"); 
+//         break;    
  
-    case 6: 
-        console.log("El promedio" + vProm +"es muy bueno"); 
-        break;      
+//     case 6: 
+//         console.log("El promedio" + vProm +"es muy bueno"); 
+//         break;      
      
-    default: 
-        console.log("Promedio no generado");     
+//     default: 
+//         console.log("Promedio no generado");     
                  
  
+// }
+
+// var vArr = ["Juan Perez", 50, 4.3, true];
+// console.log("Nombre: " + vArr[0]);
+// console.log("Edad: " + vArr[1]);
+// console.log("Promedio: " + vArr[2]);
+// console.log("Copero?: " + vArr[3]);
+
+// //ciclos iterativos
+
+// for(var x=1;x<vArr.length;x++){
+//     console.log(vArr[x]);
+// }
+
+
+//funciones
+
+function mensaje()
+{
+    console.log("Hola desde la fuinción")
+
 }
+ mensaje();
+//validación formulario
 
-var vArr = ["Juan Perez", 50, 4.3, true];
-console.log("Nombre: " + vArr[0]);
-console.log("Edad: " + vArr[1]);
-console.log("Promedio: " + vArr[2]);
-console.log("Copero?: " + vArr[3]);
-
-//ciclos iterativos
-
-for(var x=1;x<vArr.length;x++){
-    console.log(vArr[x]);
-}
 
